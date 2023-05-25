b819b03c463831082fddb6bf6d62364f7bdb0f5e
Neil Lobo <neil_edward_lobo@hotmail.com>
2023-05-25 00:17:20 -0400

    set up db connection pool

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,25 @@ if (process.env.DOTENV) {
     (await import("dotenv")).config()
 }
 
+import { createRequire } from 'node:module';
 import chalk from 'chalk'
 import express from 'express';
 import { routes } from './routes/routes.js';
+import db from 'mariadb';
+
 const app = express();
 const PORT = process.env.PORT ?? 3000;
+const require = createRequire(import.meta.url);
+const config = require("../config.json")
+
+console.log(config);
+
+const pool = db.createPool({
+    ...config.db
+})
 
 app.use(routes);
 
-app.listen(PORT, () => {
-    console.log(`${chalk.red("[API]")} Listening on ${process.env.URL}:${PORT}`);
+app.listen(PORT, async () => {
+    console.log(chalk.red("[API]"), `Listening on ${process.env.URL}:${PORT}`);
 })
