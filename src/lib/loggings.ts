dd3ce2296a956554f086777d22a8047ea737a5e5
Neil Lobo <neil_edward_lobo@hotmail.com>
2023-07-22 04:40:24 -0400

    added logging

---
+++ src/lib/loggings.ts
@@ -0,0 +1,74 @@
+import chalk from "chalk";
+
+enum Level {
+    ERROR = 0,
+    WARN = 1,
+    INFO = 2,
+    DEBUG = 3,
+}
+
+class Logger {
+    private level: Level;
+
+    constructor(initLevel: Level = Level.INFO) {
+        this.level = initLevel;
+        this.info("Logger", `Log Level: ${Level[this.level]}`);
+    }
+
+    setLevel(level: Level) {
+        this.level = level;
+    }
+
+    private log(text: string, object?: object) {
+        console.log(chalk.yellow(new Date().toLocaleString()), text);
+        if (object) {
+            console.log(object);
+        }
+    }
+
+    error(label: string, text: string, object?: object) {
+        if (this.level >= Level.ERROR) {
+            this.log(
+                `${chalk.bgRed.bold("[ERROR]")}` +
+                    chalk.green(` <${label}> `) +
+                    `${text}`,
+                object
+            );
+        }
+    }
+
+    warn(label: string, text: string, object?: object) {
+        if (this.level >= Level.WARN) {
+            this.log(
+                `${chalk.bgYellow.bold("[WARN]")}` +
+                    chalk.green(` <${label}> `) +
+                    `${text}`,
+                object
+            );
+        }
+    }
+
+    info(label: string, text: string, object?: object) {
+        if (this.level >= Level.INFO) {
+            this.log(
+                `${chalk.bgBlue.bold("[INFO]")}` +
+                    chalk.green(` <${label}> `) +
+                    `${text}`,
+                object
+            );
+        }
+    }
+
+    debug(label: string, text: string, object?: object) {
+        if (this.level >= Level.DEBUG) {
+            this.log(
+                `${chalk.bgMagenta.bold("[DEBUG]")}` +
+                    chalk.green(` <${label}> `) +
+                    `${text}`,
+                object
+            );
+        }
+    }
+}
+
+export const logger = new Logger(Number(process.env.LOG_LEVEL ?? 2) as Level);
