7a2ab3d17722ef09c53b88493c2b1486db32556d
Neil Lobo <neil_edward_lobo@hotmail.com>
2023-07-21 21:15:48 -0400

    initial redeem endpoint

---
+++ src/routes/redeem.ts
@@ -0,0 +1,21 @@
+import { Request, Response, Router, json } from "express";
+import { BAD_REQUEST } from "../lib/http-status.js";
+import { logRoute } from "../lib/middleware/route-logger.js";
+
+const router = Router();
+
+const middleware = [json(), logRoute]
+
+router.post("/redeem", middleware, (req: Request, res: Response) => {
+    if (req.body.code === undefined) {
+        return res.status(400).json({
+            ...BAD_REQUEST,
+            message: "Missing `code` field"
+        })
+    }
+
+    res.send(req.body.code)
+
+})
+
+export { router as redeem }
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,9 +2,11 @@ import { Router } from "express";
 import { signup } from "./signup.js";
 import { confirm } from "./confirm.js";
 import { login } from "./login.js";
+import { redeem } from "./redeem.js";
 
 export const routes = Router();
 
 routes.use(signup);
 routes.use(confirm);
 routes.use(login);
+routes.use(redeem);
